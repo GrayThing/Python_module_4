e30c72f38319958dc96a7745328cf425279df606
GrayThing <samorodov-artem@mail.ru>
2024-08-25 09:27:08 +0300

    Выполнено задание 2023/10/14 00:00|Домашняя работа по уроку "Модули и пакеты"

---
+++ module_4_1.py
@@ -0,0 +1,7 @@
+from fake_math import divide as fake_divide
+from true_math import divide as  true_divide
+
+print(fake_divide(57, 5))
+print(fake_divide(5, 0))
+print(true_divide(17, 15))
+print(true_divide(79, 0))
